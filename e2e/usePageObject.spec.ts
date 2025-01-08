c2cd3ea31c0d083e43f1e88219a5d6270f8e9176
Pranit Khadilkar <pranit@emtech.com>
2025-01-08 11:07:43 +0530

    added parameterized methods to submit form

--- e2e/usePageObject.spec.ts
+++ e2e/usePageObject.spec.ts
@@ -1,5 +1,6 @@
 import test from '@playwright/test'
 import { NavigationPage } from '../page-object/navigationPage'
+import { FormLayoutsPage } from '../page-object/formLayoutsPage'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:4200')
@@ -12,4 +13,17 @@ test('Navigate to form page', async ({ page }) => {
   await navigateTo.smartTablePage()
   await navigateTo.toastrPage()
   await navigateTo.tooltipPage()
+})
+
+test('parameterized method', async ({ page }) => {
+  const navigateTo = new NavigationPage(page)
+  const onFormLayoutPage = new FormLayoutsPage(page)
+
+  await navigateTo.formLayoutPage()
+  await onFormLayoutPage.submitUsingTheGridForm(
+    'test@test.com',
+    'password',
+    'Option 1'
+  )
+  await onFormLayoutPage.submitInlineForm('test', 'test@test.com', true)
 })
---
+++ page-object/formLayoutsPage.ts
@@ -0,0 +1,41 @@
+import { Page } from '@playwright/test'
+
+export class FormLayoutsPage {
+  readonly page: Page
+
+  constructor(page: Page) {
+    this.page = page
+  }
+
+  async submitUsingTheGridForm(
+    email: string,
+    password: string,
+    option: string
+  ) {
+    const usingTheGridFormCard = this.page.locator('nb-card', {
+      hasText: 'Using the Grid',
+    })
+    await usingTheGridFormCard
+      .getByRole('textbox', { name: 'Email' })
+      .fill(email)
+    await usingTheGridFormCard
+      .getByRole('textbox', { name: 'Password' })
+      .fill(password)
+    await usingTheGridFormCard
+      .getByRole('radio', { name: option })
+      .check({ force: true })
+    await usingTheGridFormCard.getByRole('button').click()
+  }
+
+  async submitInlineForm(name: string, email: string, rememberMe: boolean) {
+    const inlineFormCard = this.page.locator('nb-card', {
+      hasText: 'Inline form',
+    })
+    await inlineFormCard.getByRole('textbox', { name: 'Jane Doe' }).fill(name)
+    await inlineFormCard.getByRole('textbox', { name: 'Email' }).fill(email)
+    await inlineFormCard
+      .getByRole('checkbox', { name: 'Remember me' })
+      .check({ force: true })
+    await inlineFormCard.getByRole('button').click()
+  }
+}
